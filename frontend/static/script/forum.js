6e07e0410325fd3074b10b5ba4ab7ac135bf3e41
Chih Pin Chien <ryohei3364@gmail.com>
2025-07-06 21:18:10 +0800

    setup CI/CD

--- frontend/static/script/forum.js
+++ frontend/static/script/forum.js
@@ -6,9 +6,4 @@ import { switchLang } from './language.js';
 document.addEventListener("DOMContentLoaded", async () => {
   await initNavbar();   // 等 navbar 初始化
   switchLang();         // 綁定語言選單
-
-  // 等畫面渲染完成後再 alert
-  setTimeout(() => {
-    alert("🔧 討論區實作進行中");
-  }, 100); // 100ms 就夠讓畫面跑出來了
 });
